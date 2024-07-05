6209341b1d06fcdfbaa8932fea7e2d74a0748091
Youssef Rbahi <72280234+YoussefRbahi@users.noreply.github.com>
2024-07-05 19:18:26 +0300

    Add general styling and link to personal website

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,21 @@ function App() {
   }, [selectedFont, inputText]);
 
   return (
-    <div className="w-80 h-80 ">
-      <label htmlFor="input-textarea">
-        Enter text:
+    <div className="w-80 m-0 rounded-full p-4 grid gap-4 text-sm">
+      <div className="grid gap-2">
+        <label htmlFor="input-textarea" className="text-lg">
+          Enter text:
+        </label>
         <textarea
           name="userInput"
           id="input-textarea"
-          className="border w-full h-40"
+          className="border w-full h-20 px-2 py-0.5 resize-none"
           onChange={(e) => {
             setInputText(e.target.value);
           }}
         ></textarea>
-      </label>
+      </div>
+
       {/* Radio buttons to select font */}
       <div className="flex gap-2">
         {fonts.map((font) => (
@@ -45,33 +48,53 @@ function App() {
               checked={selectedFont === font.id}
               onChange={() => setSelectedFont(font.id)}
             />
-            <label htmlFor={font.id}>{font.name}</label>
+            <label htmlFor={font.id}>
+              {toUnicodeVariant(font.name, font.id)}
+            </label>
           </div>
         ))}
       </div>
-      <label htmlFor="output-textarea">
-        Formatted Text:
+      <div className="grid gap-2">
+        <label htmlFor="output-textarea" className="text-lg">
+          Formatted Text:
+        </label>
         <textarea
           name="userOutput"
           id="output-textarea"
-          className="border w-full h-40"
+          className="border w-full h-20 px-2 py-0.5 resize-none"
           value={formattedText}
           readOnly
         ></textarea>
-      </label>
-      <CopyToClipboard copiableText={formattedText} />
+      </div>
+      <div className="flex justify-between">
+        <CopyToClipboard copiableText={formattedText} font={selectedFont} />
+        <a
+          href="https://rbahi.com"
+          target="_blank"
+          rel="noopener"
+          className="text-lg"
+        >
+          ⓘ
+        </a>
+      </div>
     </div>
   );
 }
 
-function CopyToClipboard({ copiableText }: { copiableText: string }) {
+function CopyToClipboard({
+  copiableText,
+  font,
+}: {
+  copiableText: string;
+  font: string;
+}) {
   const [isCopied, setIsCopied] = useState(false);
 
   const copyToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
       setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 1000); // Reset copied state after 1 second
+      setTimeout(() => setIsCopied(false), 2000); // Reset copied state after 2 seconds
     } catch (err) {
       console.error("Failed to copy text: ", err);
     }
@@ -80,14 +103,14 @@ function CopyToClipboard({ copiableText }: { copiableText: string }) {
   return (
     <button
       disabled={isCopied}
-      className={`text-center transition-all duration-100 bg-blue-500 hover:bg-blue-700 text-white font-bold w-40 py-2 px-4 rounded ${
+      className={`text-center transition-all duration-100 bg-blue-500 hover:bg-blue-700 text-white font-bold w-32 py-2 px-4 rounded text-nowrap ${
         isCopied && "bg-slate-300  hover:bg-slate-300 text-green-800"
       }`}
       onClick={() => {
         copyToClipboard(copiableText);
       }}
     >
-      {isCopied ? "Done! ✓" : " Copy text"}
+      {toUnicodeVariant(isCopied ? "✅ Done!" : "📋 Copy text", font)}
     </button>
   );
 }
