3d9fddf552a655faea02fc9c4fda4bbe3e6d530b
Youssef Rbahi <72280234+YoussefRbahi@users.noreply.github.com>
2024-07-05 15:26:10 +0300

    Add option to select font

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import "./App.css";
 import { string_to_unicode_variant as toUnicodeVariant } from "string-to-unicode-variant";
 import { useEffect, useState } from "react";
 
+interface Font {
+  name: string;
+  id: string;
+}
 function App() {
-  interface MainFont {
-    name: string;
-    id: string;
-  }
-  const mainFonts: MainFont[] = [
-    { name: "bold", id: "b" },
-    { name: "italic", id: "i" },
-    { name: "boldItalic", id: "bi" },
+  const fonts: Font[] = [
+    { name: "Bold", id: "b" },
+    { name: "Italic", id: "i" },
+    { name: "Bold Italic", id: "bi" },
   ];
+  const [inputText, setInputText] = useState<string>("");
   const [formattedText, setFormattedText] = useState<string>("");
   const [selectedFont, setSelectedFont] = useState<string>("b");
 
-  const formatText = (text: string) => {
-    const newText = toUnicodeVariant(text, selectedFont);
-    setFormattedText(newText);
-  };
+  useEffect(() => {
+    setFormattedText(toUnicodeVariant(inputText, selectedFont));
+  }, [selectedFont, inputText]);
 
   return (
     <div className="w-80 h-80 ">
@@ -29,10 +29,26 @@ function App() {
           id="input-textarea"
           className="border w-full h-40"
           onChange={(e) => {
-            formatText(e.target.value);
+            setInputText(e.target.value);
           }}
         ></textarea>
       </label>
+      {/* Radio buttons to select font */}
+      <div className="flex gap-2">
+        {fonts.map((font) => (
+          <div key={font.id} className="flex gap-2">
+            <input
+              type="radio"
+              id={font.id}
+              name="selected-font"
+              value={font.id}
+              checked={selectedFont === font.id}
+              onChange={() => setSelectedFont(font.id)}
+            />
+            <label htmlFor={font.id}>{font.name}</label>
+          </div>
+        ))}
+      </div>
       <label htmlFor="output-textarea">
         Formatted Text:
         <textarea
